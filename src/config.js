9fe2379889aa4210e78c8f0d4dc1a33de590099d
himanshukrabc <himanshukrabc@gmail.com>
2023-12-11 01:01:20 +0530

    updated api endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL : "https://hkbapi.herokuapp.com/api"
+    baseURL : "https://recipebook-service.onrender.com/api/"
 });
